8c6ee35505296a231d00e8a1072d14c3deac8940
MichaelEniolade <wisdomeniolade@hotmail.com>
2016-02-21 16:09:43 +0100

    First commit

---
+++ tournament.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# 
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM games")
+    db.commit()
+    db.close
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM players")
+    db.commit()
+    db.close
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT count(*) AS num FROM players")
+    count = c.fetchone()[0]
+    db.close
+    return count
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+  
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+  
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    db = connect()
+    c = db.cursor()
+    query = "INSERT INTO players (name) VALUES (%s);"
+    args = (name,)
+    c.execute(query, args)
+    db.commit()
+    db.close
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT * FROM standings ORDER BY wins DESC;")
+    standings = c.fetchall()
+    db.close()
+    return standings
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    c = db.cursor()
+    query = "INSERT INTO games (winner, loser) VALUES (%s, %s);"
+    args = (winner, loser,)
+    c.execute(query, args)
+    db.commit()
+    db.close()
+ 
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    # Set up our variables for pairs, player getting a bye, and the bye pairing
+    pairs = []
+    byePlayer = False
+    byePair = False
+
+    # Use the playerStandings function to get our standings.
+    standings = playerStandings()
+    num_players = len(standings)
+
+    # If we have an odd number of players, we have to assign a bye
+    if 0 != num_players % 2:
+        byePlayer = assignBye()
+
+    # Loop through every other player in the standings and pair them with
+    # the next adjacent player, handle byes when we encounter them.
+    i = 0
+    while i < num_players:
+        # If we have a bye and it's been assigned to one of the next two
+        # players in the standings, then pair the bye accordingly.
+        if byePlayer and (byePlayer[0] == standings[i][0] or
+           byePlayer[0] == standings[i+1][0]):
+
+            # If the first player in the loop has a bye, create the bye pairing
+            # and pair the next two players in the standings
+            if byePlayer[0] == standings[i][0]:
+                # Our bye pairing
+                byePair = (
+                    standings[i][0],
+                    standings[i][1],
+                    '',
+                    "bye"
+                )
+
+                # Skip ahead one since we've only paired one and start over.
+                i += 1
+                continue
+
+            # If the second player in the loop has a bye, pair the first and
+            # third players and create the bye pair with the second player.
+            elif byePlayer[0] == standings[i+1][0]:
+                # Our next pair
+                pair = (
+                    standings[i][0],
+                    standings[i][1],
+                    standings[i+2][0],
+                    standings[i+2][1]
+                )
+
+                # Our bye pairing
+                byePair = (
+                    standings[i+1][0],
+                    standings[i+1][1],
+                    '',
+                    "bye"
+                )
+
+                # Skip ahead three since we've paired three players
+                i += 3
+
+        # If we don't have a bye or it's not being paired during this iteration
+        # just pair the next two players and move along.
+        else:
+            pair = (
+                standings[i][0],
+                standings[i][1],
+                standings[i+1][0],
+                standings[i+1][1]
+            )
+
+            # skip ahead two since we've paired two players
+            i += 2
+
+        # Append the main pair
+        pairs.append(pair)
+
+    # After the loop runs, append the bye pair if one exists
+    if byePair:
+        pairs.append(byePair)
+
+    # Convert the pairs array to a tuple as the requirements suggest.
+    tuple(pairs)
+    return pairs
+
+
---
+++ tournament.sql
@@ -0,0 +1,49 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Drop database;
+
+DROP DATABASE IF EXISTS tournament;
+
+-- Create database.
+
+CREATE DATABASE tournament;
+
+-- Connect to the DB before creating tables.
+
+\c tournament;
+
+-- Create table for players.
+
+CREATE TABLE players (id serial primary key, name text);
+
+-- Create table for games.
+
+CREATE TABLE games (
+	game_id serial primary key,
+	winner integer references players(id),
+	loser integer references players(id)
+);
+
+-- Create view to show standings.
+
+CREATE VIEW standings AS
+SELECT players.id, players.name,
+	SUM( CASE WHEN games.winner = players.id THEN 1 ELSE 0 END ) AS wins,
+	count(games.*) AS games
+FROM players LEFT JOIN games
+ON players.id = games.winner OR players.id = games.loser
+GROUP BY players.id;
+
+-- Create a view for showing the number of byes per player
+
+CREATE VIEW byes AS
+SELECT players.id, players.name, count(games.winner) AS bye_count
+FROM players LEFT JOIN games
+ON players.id = games.winner AND games.loser IS NULL
+GROUP BY players.id;
